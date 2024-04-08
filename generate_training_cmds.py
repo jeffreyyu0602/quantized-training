208e168e2ab6bffe8dab08b8d4f763a5b6928b34
Jeffrey Yu <jeffreyy@stanford.edu>
2024-04-07 22:44:45 -0700

    Update squad  fine-tuning commands

---
+++ generate_training_cmds.py
@@ -0,0 +1,48 @@
+models = [
+    'models/mobilebert_tiny',
+    'google/mobilebert-uncased',
+    'roberta-base',
+    'roberta-large',
+]
+
+configs = ["bf16", "posit8", "posit8-approx-shifted", "fp8"]
+
+tasks = ["mnli", "qnli", "mrpc", "sst2", "squad"]
+
+model_to_filename = {
+    "models/mobilebert_tiny": "mobilebert_tiny",
+    "google/mobilebert-uncased": "mobilebert_uncased",
+    "roberta-base": "roberta_base",
+    "roberta-large": "roberta_large",
+    "roberta-large-mnli": "roberta_large",
+}
+
+
+def generate_commands():
+    with open('asplos_training.sh', 'w') as file:
+        for model in models:
+            for task in tasks:
+                for config in configs:
+                    model_id = "roberta-large-mnli" if (
+                        model == "roberta-large" and task == "mrpc") else model
+                    cmd = [
+                        "python", "run_quantized_training.py",
+                        "--model", model_id,
+                        "--task", task,
+                        "--warmup_ratio", "0.06",
+                        "--run_job", config,
+                        "--log_file", f"logs/{model_to_filename[model]}-{task}-{config}.log",
+                    ]
+                    if model == "google/mobilebert-uncased":
+                        if task == "squad":
+                            cmd += ["--sgd", "--op_fusion", "qa_outputs"]
+                        else:
+                            cmd += ["--op_fusion", "classifier"]
+                    
+                    for seed in range(3):
+                        file.write(" ".join(cmd) + f" --seed {seed}\n")
+                file.write("\n")
+
+
+if __name__ == "__main__":
+    generate_commands()
