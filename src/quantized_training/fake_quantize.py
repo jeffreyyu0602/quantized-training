09622f17d04dfd9f6593cc51ae8ff92a3ffccac8
Jeffrey Yu <jeffreyy@stanford.edu>
2024-05-07 05:42:57 -0700

    Add per-vector/microscaling. Remove unused buffer from fake quant module.

--- src/quantized_training/fake_quantize.py
+++ src/quantized_training/fake_quantize.py
@@ -14,7 +14,7 @@ __all__ = [
 logger = logging.getLogger(__name__)
 
 
-def _round_to_mbits(input, mbits):
+def _round_mantissa(input, mbits):
     lb_mask = 1 << (23 - mbits)
     gb_mask = lb_mask >> 1
     sb_mask = gb_mask - 1
@@ -30,14 +30,16 @@ def _round_to_mbits(input, mbits):
     return raw_bits.view(torch.float32).to(input.dtype)
 
 
-def _clamp_exp(x, min_exp, max_exp):
+def _clamp_exp(x, ebits):
+    exp_bias = 2 ** (ebits - 1) - 1
+    min_exp, max_exp = 1 - exp_bias, 2 ** ebits - 2 - exp_bias
     exponent = torch.floor(torch.log2(x))
     x = torch.where(exponent < min_exp, 0, x)
-    x = torch.where(exponent > max_exp, 2 ** max_exp, x)
+    x = torch.where(exponent > max_exp, 2 ** (max_exp + 1), x)
     return x
 
 
-def dtype_to_fq_fn(dtype: str):
+def _get_fq_fn(dtype: str):
     """Return the quantization function for the given dtype."""
     if (match := re.fullmatch(r'posit(\d+)_(\d+)', dtype)):
         nbits, es = match.groups()
@@ -45,23 +47,45 @@ def dtype_to_fq_fn(dtype: str):
     elif (match := re.fullmatch(r'(?:FP8\.)?(E4M3|E5M2)', dtype, re.IGNORECASE)):
         fp8_format = match.group(1).lower()
         return quantize_to_fp8_e4m3 if fp8_format == 'e4m3' else quantize_to_fp8_e5m2
-    elif (match := re.fullmatch(r'E(\d+)MX', dtype, re.IGNORECASE)):
-        N = int(match.group(1))
-        bias = 2 ** (N - 1) - 1
-        min_exp, max_exp = 1 - bias, 2 ** N - 2 - bias
-        return lambda x: _clamp_exp(x, min_exp, max_exp)
-    elif (match := re.fullmatch(r'EXM(\d+)', dtype, re.IGNORECASE)):
+    elif (match := re.fullmatch(r'E8M(\d+)', dtype, re.IGNORECASE)):
         mbits = int(match.group(1))
         assert mbits >= 0 and mbits <= 23, "mbits must be between 0 and 23"
-        return lambda x: _round_to_mbits(x, mbits)
+        return lambda x: _round_mantissa(x, mbits)
+    elif (match := re.fullmatch(r'E(\d+)MY', dtype, re.IGNORECASE)):
+        ebits = int(match.group(1))
+        return lambda x: _clamp_exp(x, ebits)
+    elif dtype == "binary":
+        return lambda x: torch.sign(x - x.mean())
     else:
         raise ValueError(f"Unrecognized dtype: {dtype}")
 
 
+def _get_max_val(x, qscheme, ch_axis=1, block_size=32):
+    if qscheme == "per_tensor":
+        max_val = torch.amax(torch.abs(x))
+    elif qscheme == "per_channel":
+        y = torch.flatten(x.transpose(0, ch_axis), start_dim=1)
+        max_val = torch.amax(torch.abs(y), dim=1)
+    elif qscheme == "per_vector":
+        x_dim = x.size()
+        sp_axis = len(x_dim) - 2
+        new_x_shape = (
+            x_dim[:sp_axis] +
+            (block_size, x_dim[sp_axis] // block_size) +
+            x_dim[sp_axis + 1:]
+        )
+        y = x.view(new_x_shape)
+        max_val = torch.amax(torch.abs(y), dim=(0, sp_axis)).unsqueeze(0)
+    else:
+        raise ValueError(f"Unrecognized qscheme: {qscheme}")
+    return max_val
+
+
 class FakeQuantFunction(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, input, values, scale=None):
+    def forward(ctx, input, scale, encodings):
         if scale is not None:
+            scale = scale.to(input.dtype)
             input = input * scale
 
         if input.dtype == torch.bfloat16:
@@ -69,7 +93,7 @@ class FakeQuantFunction(torch.autograd.Function):
         else:
             raw_bits = input.float().view(torch.int32)
             indices = ((raw_bits >> 16) & 0xffff) | ((raw_bits & 0xffff) != 0).int()
-        input = values[indices].to(input.dtype)
+        input = encodings[indices].to(input.dtype)
 
         return input / scale if scale is not None else input
 
@@ -86,7 +110,7 @@ class FusedAmaxObsFakeQuantize(FakeQuantizeBase):
     parameters. The module records the maximums of absolute values of output
     tensors, and uses this statistic to compute the quantization parameters.
     """
-    value_map: torch.Tensor
+    encodings: torch.Tensor
     amax_history: torch.Tensor
     scale: torch.Tensor
     histogram: torch.Tensor
@@ -97,7 +121,10 @@ class FusedAmaxObsFakeQuantize(FakeQuantizeBase):
         qscheme: str = None,
         quant_max: float = None,
         amax_history_len: int = 50,
-        observer_enabled: bool = False,
+        ch_axis: int = 1,
+        block_size: int = 8,
+        observe_histogram: bool = False,
+        name=None,
         **kwargs,
     ) -> None:
         super().__init__()
@@ -105,20 +132,24 @@ class FusedAmaxObsFakeQuantize(FakeQuantizeBase):
         self.qscheme = qscheme
         self.quant_max = quant_max
         self.amax_history_len = amax_history_len
-        self.ch_axis = -1
-        self.group_size = 8
-        self.name = kwargs.get("name", None)
+        self.ch_axis = ch_axis
+        self.block_size = block_size
+        self.name = name
         device = kwargs.get("device", None)
-        factory_kwargs = {'device': device, 'dtype': torch.float}
-        self.register_buffer("amax_history", torch.zeros(amax_history_len, **factory_kwargs))
-        self.register_buffer('scale', torch.tensor([1.0], **factory_kwargs))
-        # Generate all possible bfloat16 values and quantize them to the given dtype.
+        # Generates a mapping from bfloat16 to quantized values of the given dtype
         input = torch.arange(2 ** 16, dtype=torch.int16, device=device).view(torch.bfloat16)
-        self.fake_quant_fn = dtype_to_fq_fn(dtype)
-        self.register_buffer("value_map", self.fake_quant_fn(input), persistent=False)
-        # Records the histogram of the input tensor
-        self.enable_observer(observer_enabled)
-        self.register_buffer("histogram", torch.zeros(254, **factory_kwargs))
+        self.fake_quant_fn = _get_fq_fn(dtype)
+        self.register_buffer("encodings", self.fake_quant_fn(input), persistent=False)
+        # Create amax history and scale buffer
+        factory_kwargs = {'device': device, 'dtype': torch.float}
+        if qscheme is not None:
+            self.register_buffer("amax_history", None)
+            self.register_buffer('scale', torch.tensor([1.0], **factory_kwargs))
+            self.enable_observer()
+        # Create histogram buffer
+        if observe_histogram:
+            self.register_buffer("histogram", torch.zeros(254, **factory_kwargs))
+        self.observe_histogram = observe_histogram
 
     @torch.jit.export
     def calculate_qparams(self):
@@ -140,30 +171,22 @@ class FusedAmaxObsFakeQuantize(FakeQuantizeBase):
         )
 
     def forward(self, X: torch.Tensor) -> torch.Tensor:
-        if self.qscheme is not None:
+        if self.observe_histogram:
+            self._combine_histograms(X)
+
+        if self.observer_enabled[0] == 1:
             x = X.detach()  # avoid keeping autograd tape
 
             # Remove attention mask when computing scaling factor
-            dtype_min = torch.finfo(X.dtype).min
-            attention_mask = torch.where(x == dtype_min, dtype_min, 0.0)
-            X -= attention_mask
+            # dtype_min = torch.finfo(X.dtype).min
+            # attention_mask = torch.where(x == dtype_min, dtype_min, 0.0)
+            # X -= attention_mask
 
-            if self.qscheme == "per_tensor":
-                max_val = torch.amax(torch.abs(x))
-            elif self.qscheme == "per_channel":
-                y = torch.flatten(x.transpose(0, self.ch_axis), start_dim=1)
-                max_val = torch.amax(torch.abs(y), dim=1)
-            elif self.qscheme == "per_vector":
-                x_dim = x.size()
-                new_x_shape = x_dim[:-2] + (self.group_size, x_dim[-2] // self.group_size, x_dim[-1])
-                y = x.view(new_x_shape)
-                axis_list = tuple(range(len(new_x_shape)))[:-2]
-                max_val = torch.amax(torch.abs(y), dim=axis_list).repeat_interleave(self.group_size, dim=0)
-            else:
-                raise ValueError(f"Unrecognized qscheme: {self.qscheme}")
-
-            if self.qscheme in ["per_channel", "per_vector"] and len(self.amax_history.size()) < 2:
-                self.amax_history = torch.zeros((self.amax_history_len, *max_val.shape), device=X.device)
+            max_val = _get_max_val(x, self.qscheme, self.ch_axis, self.block_size)
+            if self.amax_history is None:
+                self.amax_history = torch.zeros(
+                    (self.amax_history_len, *max_val.shape), device=max_val.device
+                )
 
             amax = torch.amax(self.amax_history, dim=0)
             self.amax_history = torch.roll(self.amax_history, -1, 0)
@@ -172,20 +195,16 @@ class FusedAmaxObsFakeQuantize(FakeQuantizeBase):
             sf = torch.pow(2, torch.floor(torch.log2(self.quant_max / amax)))
             self.scale = torch.where(torch.isfinite(sf), sf, self.scale)
 
-        if self.observer_enabled[0] == 1:
-            self._combine_histograms(self.histogram, X)
-
         if self.fake_quant_enabled[0] == 1:
-            if self.qscheme is not None:
-                X = FakeQuantFunction.apply(X, self.value_map, self.scale.to(X.dtype))
-                X += attention_mask
-            else:
-                X = FakeQuantFunction.apply(X, self.value_map)
+            X = FakeQuantFunction.apply(
+                X, self.scale if self.qscheme is not None else None, self.encodings
+            )
+            # X += attention_mask
 
         return X
 
-    def _combine_histograms(self, histogram: torch.Tensor, X: torch.Tensor) -> None:
+    def _combine_histograms(self, X: torch.Tensor) -> None:
         x = X.detach().float()
-        exponent = torch.floor(torch.log2(x[x != 0].abs()))
-        combined_histogram = torch.histc(exponent, 254, min=-126, max=127)
-        histogram += combined_histogram
+        exponent = torch.floor(torch.log2(torch.abs(x)))
+        hist = torch.histc(exponent, 254, min=-126, max=127)
+        self.histogram += hist
---
+++ src/quantized_training/modules/quantized/__init__.py
@@ -0,0 +1,9 @@
+from .conv import Conv1d, Conv2d, Conv3d
+from .linear import Linear
+
+__all__ = [
+    'Conv1d',
+    'Conv2d',
+    'Conv3d',
+    'Linear',
+]
