Add integer data type. Fix per-channel scaling.

src/quantized_training/fake_quantize.py
@@ -54,33 +54,34 @@ def _get_fq_fn(dtype: str):
     elif (match := re.fullmatch(r'E(\d+)MY', dtype, re.IGNORECASE)):
         ebits = int(match.group(1))
         return lambda x: _clamp_exp(x, ebits)
-    elif dtype == "binary":
-        return lambda x: torch.sign(x - x.mean())
+    elif (match := re.fullmatch(r'int(\d+)', dtype, re.IGNORECASE)):
+        nbits = int(match.group(1))
+        quant_min, quant_max = -2 ** (nbits - 1), 2 ** (nbits - 1) - 1
+        return lambda x: torch.clamp(torch.round(x), quant_min, quant_max)
     else:
         raise ValueError(f"Unrecognized dtype: {dtype}")
 
 
-def _get_max_val(x, qscheme, ch_axis=1, block_size=32):
-    if qscheme == "per_tensor":
-        max_val = torch.amax(torch.abs(x))
-    elif qscheme == "per_channel":
-        y = torch.flatten(x.transpose(0, ch_axis), start_dim=1)
-        max_val = torch.amax(torch.abs(y), dim=1)
-    elif qscheme == "per_vector":
-        x_dim = x.size()
-        sp_axis = len(x_dim) - 2
+def  _get_amax(x, qscheme, axis=1, block_size=32):
+    if qscheme.value == "per_tensor":
+        amax = torch.amax(torch.abs(x))
+    elif qscheme.value == "per_channel":
+        dim = tuple(range(x.dim()))
+        dim = dim[:axis] + dim[axis + 1:]
+        amax = torch.amax(torch.abs(x), dim=dim, keepdim=True)
+    elif qscheme.value == "per_vector":
+        x_shape = x.size()
         new_x_shape = (
-            x_dim[:sp_axis] +
-            (block_size, x_dim[sp_axis] // block_size) +
-            x_dim[sp_axis + 1:]
+            x_shape[:axis] +
+            (block_size, x_shape[axis] // block_size) +
+            x_shape[axis + 1:]
         )
         y = x.view(new_x_shape)
-        max_val = torch.amax(torch.abs(y), dim=(0, sp_axis)).unsqueeze(0)
-    else:
-        raise ValueError(f"Unrecognized qscheme: {qscheme}")
-    return max_val
+        amax = torch.amax(torch.abs(y), dim=(0, axis)).unsqueeze(0)
+    return amax
 
 
+# TODO: perform fake quantization directly for simple data types
 class FakeQuantFunction(torch.autograd.Function):
     @staticmethod
     def forward(ctx, input, scale, encodings):
@@ -122,7 +123,7 @@ class FusedAmaxObsFakeQuantize(FakeQuantizeBase):
         quant_max: float = None,
         amax_history_len: int = 50,
         ch_axis: int = 1,
-        block_size: int = 8,
+        block_size: int = 16,
         record_histogram: bool = False,
         name=None,
         **kwargs,
@@ -176,26 +177,27 @@ class FusedAmaxObsFakeQuantize(FakeQuantizeBase):
         )
 
     def forward(self, X: torch.Tensor) -> torch.Tensor:
+        # Remove attention mask when computing scaling factor
+        # if self.qscheme is not None:
+        #     dtype_min = torch.finfo(X.dtype).min
+        #     attention_mask = torch.where(x == dtype_min, dtype_min, 0.0)
+        #     X -= attention_mask
+
         if self.record_histogram:
             self._combine_histograms(X)
 
         if self.observer_enabled[0] == 1:
             x = X.detach()  # avoid keeping autograd tape
 
-            # Remove attention mask when computing scaling factor
-            # dtype_min = torch.finfo(X.dtype).min
-            # attention_mask = torch.where(x == dtype_min, dtype_min, 0.0)
-            # X -= attention_mask
-
-            max_val = _get_max_val(x, self.qscheme, self.ch_axis, self.block_size)
+            curr_amax = _get_amax(x, self.qscheme, self.ch_axis, self.block_size)
             if self.amax_history is None:
                 self.amax_history = torch.zeros(
-                    (self.amax_history_len, *max_val.shape), device=max_val.device
+                    (self.amax_history_len, *curr_amax.shape), device=curr_amax.device
                 )
 
             amax = torch.amax(self.amax_history, dim=0)
             self.amax_history = torch.roll(self.amax_history, -1, 0)
-            self.amax_history[0] = max_val.float()
+            self.amax_history[0] = curr_amax.float()
 
             sf = torch.pow(2, torch.floor(torch.log2(self.quant_max / amax)))
             self.scale = torch.where(torch.isfinite(sf), sf, self.scale)
@@ -204,12 +206,13 @@ class FusedAmaxObsFakeQuantize(FakeQuantizeBase):
             X = FakeQuantFunction.apply(
                 X, self.scale if self.qscheme is not None else None, self.encodings
             )
-            # X += attention_mask
+
+        # if self.qscheme is not None:
+        #     X += attention_mask
 
         return X
 
     def _combine_histograms(self, X: torch.Tensor) -> None:
         x = X.detach().float()
-        exponent = torch.floor(torch.log2(torch.abs(x)))
-        hist = torch.histc(exponent, 254, min=-126, max=127)
-        self.histogram += hist
+        exp = torch.floor(torch.log2(torch.abs(x)))
+        self.histogram += torch.histc(exp, 254, min=-126, max=127)

src/quantized_training/quantize.py
@@ -1,7 +1,5 @@
 import copy
 import logging
-import re
-from typing import List
 
 import torch
 import torch.nn as nn
@@ -42,42 +40,27 @@ def propagate_config(module, name, qconfig):
     for child in module.children():
         propagate_config(child, name, qconfig)
 
+def _create_fake_quant(qconfig, args):
+    if qconfig is None:
+        return nn.Identity()
+
+    return FusedAmaxObsFakeQuantize.with_args(
+        **qconfig.to_dict(), record_histogram=args.record_histogram
+    )
+
 def quantize(model, args, run_fn=None, inplace=True):
     if not inplace:
         model = copy.deepcopy(model)
 
-    if "," in args.dtype:
-        dtype_fwd, dtype_bwd = args.dtype.split(",")
-    elif re.search(r'^FP8(\.MIXED)?$', args.dtype, re.IGNORECASE):
-        dtype_fwd, dtype_bwd = ("E4M3", "E5M2")
-    else:
-        dtype_fwd, dtype_bwd = (args.dtype, args.dtype)
-
-    default_fake_quant = FusedAmaxObsFakeQuantize.with_args(
-        dtype=dtype_fwd,
-        qscheme=args.scaling_fwd[0],
-        quant_max=args.scaling_fwd[1],
-        amax_history_len=args.scaling_fwd[2],
-        record_histogram=args.record_histogram
-    )
-
-    error_fake_quant = FusedAmaxObsFakeQuantize.with_args(
-        dtype=dtype_bwd,
-        qscheme=args.scaling_bwd[0],
-        quant_max=args.scaling_bwd[1],
-        amax_history_len=args.scaling_bwd[2],
-        record_histogram=args.record_histogram
-    )
-
-    # TODO: weights need separate quantization parameters
+    act_fake_quant = _create_fake_quant(args.activation, args)
+    wt_fake_quant = _create_fake_quant(args.weight, args)
+    error_fake_quant = _create_fake_quant(args.error, args)
     qconfig = QConfig(
-        activation=default_fake_quant if args.quantize_fwd else nn.Identity,
-        weight=default_fake_quant if args.quantize_weight else nn.Identity,
-        error=error_fake_quant if args.quantize_bwd else nn.Identity,
+        activation=act_fake_quant, weight=wt_fake_quant, error=error_fake_quant
     )
 
     if (
-        (args.quantize_fwd or args.quantize_bwd or args.posit_exp or
+        (args.activation or args.error or args.posit_exp or
             args.posit_exp_shifted or args.posit_reciprocal) and
         hasattr(model, 'config') and
         isinstance(model.config, PretrainedConfig)
@@ -97,16 +80,27 @@ def quantize(model, args, run_fn=None, inplace=True):
             dtype=torch.bfloat16 if args.bf16 else None,
         )
 
+    # TODO: better way to handle this?
+    if args.activation is None:
+        args.quantize_forward = None
+    if args.error is None:
+        args.quantize_backprop = None
+
     propagate_config(model, 'qconfig', qconfig)
     convert(model, DEFAULT_QAT_MODULE_MAPPINGS, inplace=True)
-    prepare(model, True, args.quantize_fwd, args.quantize_bwd, args.op_fusion)
+    prepare(model, True, args.quantize_forward, args.quantize_backprop, args.op_fusion)
 
     if run_fn is not None:
         run_fn(model)
 
-    # FIXME: use a cofig dict instead of argparse Namespace
+    # TODO: use a config dict instead of argparse Namespace
     if hasattr(args, 'bf16') and args.bf16:
         model.bfloat16()
+
+    # ASPLOS experiments perform quantization after converting model dtype to bfloat16
+    # for name, param in model.named_parameters():
+    #     if not 'bias' in name:
+    #         param.data = qconfig.weight(device=param.device)(param.data)
 
     return model
 
@@ -124,7 +118,8 @@ def _get_unique_devices_(mod):
         {p.device for p in mod.buffers()}
 
 def _register_module_hook(module, name, hook_name):
-    def _observer_pre_hook(self, inputs):
+    module.add_module(hook_name, nn.ModuleDict())
+    def _forward_pre_hook(self, inputs):
         new_inputs = []
         module_dict = getattr(self, hook_name)
         for i, input in enumerate(inputs):
@@ -132,23 +127,24 @@ def _register_module_hook(module, name, hook_name):
                 new_inputs.append(input)
                 continue
 
-            if (idx := str(i)) not in module_dict:
+            if (obs_or_fq_name := str(i)) not in module_dict:
                 obs_or_fq = getattr(self.qconfig, hook_name.split('_')[0])
                 module_dict.update({
-                    idx: obs_or_fq(device=input.device, name=name)
+                    obs_or_fq_name: obs_or_fq(device=input.device, name=name)
                 })
-            new_inputs.append(module_dict[idx](input))
+            new_inputs.append(module_dict[obs_or_fq_name](input))
         return tuple(new_inputs)
 
-    module.add_module(hook_name, nn.ModuleDict())
+    def _backward_hook(self, grad_inputs, grad_outputs):
+        return _forward_pre_hook(self, grad_inputs)
+
     if hook_name == 'activation_pre_process':
-        module.register_forward_pre_hook(_observer_pre_hook)
+        module.register_forward_pre_hook(_forward_pre_hook)
     elif hook_name == 'error_pre_process':
-        module.register_full_backward_pre_hook(_observer_pre_hook)
+        # backward pre hook has the same signature as forward pre hook
+        module.register_full_backward_pre_hook(_forward_pre_hook)
     elif hook_name == 'error_post_process':
-        def _observer_backward_hook(self, grad_inputs, grad_outputs):
-            return _observer_pre_hook(self, grad_inputs)
-        module.register_full_backward_hook(_observer_backward_hook)
+        module.register_full_backward_hook(_backward_hook)
 
 def _add_observer_(
         module, fwd_pre_hook_module_list, bwd_pre_hook_module_list,
@@ -181,18 +177,13 @@ def _add_observer_(
     _insert_obs_or_fq(module, prefix)
 
 def prepare(
-    model: Module,
-    inplace=False,
-    quantize_fwd: str = None,
-    quantize_bwd: str = None,
-    op_fusion: List[str] = None
-) -> Module:
+        model, inplace=False, ops_fwd=None, ops_bwd=None, op_fusion=None):
     if not inplace:
         model = copy.deepcopy(model)
 
     _add_observer_(
-        model, _parse_ops(quantize_fwd), _parse_ops(quantize_bwd),
-        quantize_bwd is not None and "residual" in quantize_bwd, op_fusion, '')
+        model, _parse_ops(ops_fwd), _parse_ops(ops_bwd),
+        ops_bwd and "residual" in ops_bwd, op_fusion, '')
     return model
 
 def convert(module, mapping=None, inplace=False, custom_module_class_mapping=None):

src/quantized_training/quantize_pt2e.py
@@ -13,82 +13,30 @@ from .qconfig import QConfig
 from .quantization_mappings import QUANTIZATION_OPERATORS
 
 
-def _quantize_weight(float_wt, quant_min=-128, quant_max=127):
-    min_val, max_val = torch.aminmax(float_wt)
-    max_val = torch.max(-min_val, max_val)
-    scale = max_val / (float(quant_max - quant_min) / 2)
-    return torch.clamp(torch.round(float_wt / scale), quant_min, quant_max) * scale
+def _create_fake_quant(qconfig, args):
+    if qconfig is None:
+        return nn.Identity()
+
+    return FusedAmaxObsFakeQuantize.with_args(
+        **qconfig.to_dict(), record_histogram=args.record_histogram
+    )
 
 
-def quantize_pt2e(model, args, example_args, example_kwargs=None, dynamic_shapes=None, run_fn=None):
-    if args.dtype in ["qint8", "quint8"]:
-        from torch.ao.quantization import (
-            MovingAverageMinMaxObserver,
-            MovingAveragePerChannelMinMaxObserver,
-            FusedMovingAvgObsFakeQuantize,
-            default_weight_observer,
-        )
-        from torch.ao.quantization.fake_quantize import _is_per_channel
-        qscheme = (
-            getattr(torch, args.scaling_fwd[0])
-            if args.scaling_fwd[0] is not None
-            else torch.per_tensor_affine
-        )
-        observer = (
-            MovingAveragePerChannelMinMaxObserver
-            if _is_per_channel(qscheme) else MovingAverageMinMaxObserver
-        )
-        default_act_fake_quant = FusedMovingAvgObsFakeQuantize.with_args(
-            observer=observer,
-            ch_axis=1,
-            quant_min=-128,
-            quant_max=127,
-            dtype=getattr(torch, args.dtype),
-            qscheme=qscheme,
-        )
-        qconfig = torch.ao.quantization.QConfig(
-            activation=default_act_fake_quant if args.quantize_fwd else nn.Identity,
-            weight=default_weight_observer if args.quantize_weight else nn.Identity,
-        )
+def quantize_pt2e(
+        model, args, example_args, example_kwargs=None, dynamic_shapes=None,
+        run_fn=None):
+    act_fake_quant = _create_fake_quant(args.activation, args)
+    wt_fake_quant = _create_fake_quant(args.weight, args)
+    error_fake_quant = _create_fake_quant(args.error, args)
+    qconfig = QConfig(
+        activation=act_fake_quant, weight=wt_fake_quant, error=error_fake_quant
+    )
 
-        for name, param in model.named_parameters():
-            if not 'bias' in name:
-                param.data = _quantize_weight(param.data, -128, 127)
-    else:
-        if "," in args.dtype:
-            dtype_fwd, dtype_bwd = args.dtype.split(",")
-        elif re.search(r'^FP8(\.MIXED)?$', args.dtype, re.IGNORECASE):
-            dtype_fwd, dtype_bwd = ("E4M3", "E5M2")
-        else:
-            dtype_fwd, dtype_bwd = (args.dtype, args.dtype)
+    for name, param in model.named_parameters():
+        if not 'bias' in name:
+            param.data = qconfig.weight(device=param.device)(param.data)
 
-        default_fake_quant = FusedAmaxObsFakeQuantize.with_args(
-            dtype=dtype_fwd,
-            qscheme=args.scaling_fwd[0],
-            quant_max=args.scaling_fwd[1],
-            amax_history_len=args.scaling_fwd[2],
-            record_histogram=args.record_histogram
-        )
-
-        error_fake_quant = FusedAmaxObsFakeQuantize.with_args(
-            dtype=dtype_bwd,
-            qscheme=args.scaling_bwd[0],
-            quant_max=args.scaling_bwd[1],
-            amax_history_len=args.scaling_bwd[2],
-            record_histogram=args.record_histogram
-        )
-
-        qconfig = QConfig(
-            activation=default_fake_quant if args.quantize_fwd else nn.Identity,
-            weight=default_fake_quant if args.quantize_weight else nn.Identity,
-            error=error_fake_quant if args.quantize_bwd else nn.Identity,
-        )
-
-        for name, param in model.named_parameters():
-            if not 'bias' in name:
-                param.data = qconfig.weight(device=param.device)(param.data)
-
-    ops = args.quantize_fwd.split(',') if args.quantize_fwd is not None else []
+    ops = args.quantize_forward.split(',')
     node_list = tuple(node for op in ops for node in QUANTIZATION_OPERATORS[op.lower()])
 
     # torch.export does not support training due to inplace operations