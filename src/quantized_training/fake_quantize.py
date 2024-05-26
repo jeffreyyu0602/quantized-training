Fix per-vector scaling negative axis. Fix conv2d not being quantized.

src/quantized_training/fake_quantize.py
@@ -160,7 +160,7 @@ class FusedAmaxObsFakeQuantFunction(torch.autograd.Function):
                 input, axes, orig_shape, padded_shape = _reshape_to_blocks(
                     input, axes, block_size
                 )
-            ch_axis = ch_axis + 1
+            ch_axis = axes[0] + 1
 
         if observer_enabled[0] == 1:
             curr_amax = _get_amax(input, qscheme, ch_axis)

src/quantized_training/quantize_pt2e.py
@@ -43,31 +43,23 @@ def prepare_pt2e(
         dynamic_shapes=dynamic_shapes,
     )
 
-    # TODO: right now only check activation qscheme
-    if args.activation is not None and args.activation.qscheme == QScheme.MICROSCALING:
-        act_qspec = replace(args.activation, ch_axis=-1)
-        weight_qspec = replace(args.weight, ch_axis=-1)
-        linear_qonfig = QuantizationConfig(act_qspec, None, weight_qspec, None)
+    act_qspec = replace(args.activation, ch_axis=-1) if args.activation else None
+    weight_qspec = replace(args.weight, ch_axis=-1) if args.weight else None
+    linear_qonfig = QuantizationConfig(act_qspec, None, weight_qspec, None)
 
-        act_qspec = replace(args.activation, ch_axis=1)
-        weight_qspec = replace(args.weight, ch_axis=1)
-        conv2d_qonfig = QuantizationConfig(act_qspec, None, weight_qspec, None)
+    act_qspec = replace(args.activation, ch_axis=1) if args.activation else None
+    weight_qspec = replace(args.weight, ch_axis=1) if args.weight else None
+    conv2d_qonfig = QuantizationConfig(act_qspec, None, weight_qspec, None)
 
-        act_qspec = replace(args.activation, ch_axis=-1)
-        weight_qspec = replace(args.activation, ch_axis=-2)
-        matmul_qonfig = QuantizationConfig(act_qspec, None, weight_qspec, None)
+    # Matmul use weight qspec for the second input
+    act_qspec = replace(args.activation, ch_axis=-1) if args.activation else None
+    weight_qspec = replace(args.activation, ch_axis=-2) if args.activation else None
+    matmul_qonfig = QuantizationConfig(act_qspec, None, weight_qspec, None)
 
-        quantizer = (XNNPACKQuantizer()
-                    .set_operator_type(torch.ops.aten.linear.default, linear_qonfig)
-                    .set_operator_type(torch.ops.aten.conv2d.default, conv2d_qonfig)
-                    .set_operator_type(torch.ops.aten.matmul.default, matmul_qonfig))
-    else:
-        qconfig = QuantizationConfig(args.activation, None, args.weight, None)
-        # Matmul use weight qspec for the second input
-        matmul_qonfig = replace(qconfig, weight=args.activation)
-        quantizer = (XNNPACKQuantizer()
-                    .set_global(qconfig)
-                    .set_operator_type(torch.ops.aten.matmul.default, matmul_qonfig))
+    quantizer = (XNNPACKQuantizer()
+                 .set_module_type(torch.nn.Linear, linear_qonfig)
+                 .set_module_type(torch.nn.Conv2d, conv2d_qonfig)
+                 .set_operator_type(torch.ops.aten.matmul.default, matmul_qonfig))
 
     prepare_pt2e(model, quantizer)
     model.recompile()

src/quantized_training/quantizer/xnnpack_quantizer_utils.py
@@ -187,36 +187,32 @@ def _annotate_matmul(
     annotated_partitions = []
     input_act_qspec = quantization_config.input_activation
     output_act_qspec = quantization_config.output_activation
+    weight_qspec = quantization_config.weight
     for node in gm.graph.nodes:
         if node.op != "call_function" or node.target != torch.ops.aten.matmul.default:
             continue
-        matmul_node = node
-        if filter_fn and not filter_fn(matmul_node):
+        if filter_fn and not filter_fn(node):
             continue
-        if _is_annotated([matmul_node]):
+        if _is_annotated([node]):
             continue
-
-        input_act_qspec = quantization_config.input_activation
-        output_act_qspec = quantization_config.output_activation
-        weight_qspec = quantization_config.weight
 
         input_qspec_map = {}
-        input_act0 = matmul_node.args[0]
+        input_act0 = node.args[0]
         if isinstance(input_act0, Node):
             input_qspec_map[input_act0] = input_act_qspec
 
         # We use weight_qspec for the second input to differentiate
-        # the two inputs to the matmul
-        input_act1 = matmul_node.args[1]
+        # the two inputs of torch.matmul
+        input_act1 = node.args[1]
         if isinstance(input_act1, Node):
             input_qspec_map[input_act1] = weight_qspec
 
-        matmul_node.meta["quantization_annotation"] = QuantizationAnnotation(
+        node.meta["quantization_annotation"] = QuantizationAnnotation(
             input_qspec_map=input_qspec_map,
             output_qspec=output_act_qspec,
             _annotated=True,
         )
-        annotated_partitions.append(matmul_node)
+        annotated_partitions.append(node)
     return annotated_partitions
 
 