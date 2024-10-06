Fix softmax operation mapping. Rework reshape fusion. Bug fix.

src/quantized_training/codegen/mapping.py
@@ -480,24 +480,25 @@ def fuse_operator(model: GraphModule, pipeline=None):
             fused_nodes[n] = None
 
         # Switch order of transpose and select nodes
-        if group[0].target == torch.ops.aten.transpose.int:
-            transpose_node = group.pop(0)
-            next_node = next(iter(transpose_node.users))
-            unfuse_nodes = []
+        reshape_node = group[0]
+        if reshape_node.target == torch.ops.aten.transpose.int:
+            next_node = next(iter(reshape_node.users))
+            unfused_nodes = []
             while next_node.target == torch.ops.aten.select.int:
-                unfuse_nodes.append(next_node)
+                unfused_nodes.append(next_node)
                 next_node = next(iter(next_node.users))
 
-            if len(unfuse_nodes) > 0:
+            if len(unfused_nodes) > 0:
                 with graph.inserting_before(next_node):
-                    new_node = graph.node_copy(transpose_node, lambda _: unfuse_nodes[-1])
-                next_node.replace_input_with(unfuse_nodes[-1], new_node)
-                unfuse_nodes[0].replace_input_with(transpose_node, transpose_node.args[0])
+                    new_node = graph.node_copy(reshape_node, lambda _: unfused_nodes[-1])
+                next_node.replace_input_with(unfused_nodes[-1], new_node)
+                unfused_nodes[0].replace_input_with(reshape_node, reshape_node.args[0])
 
-                graph.erase_node(transpose_node)
+                graph.erase_node(reshape_node)
 
-                for n in unfuse_nodes:
+                for n in unfused_nodes:
                     group.remove(n)
+                group.remove(reshape_node)
                 group.insert(0, new_node)
 
     partitions = get_source_partitions(graph, [torch.ops.quantized_ops.dequantize_symmetric])
@@ -508,21 +509,29 @@ def fuse_operator(model: GraphModule, pipeline=None):
         # If the node is already fused, skip it
         if search_group(node) is not None:
             continue
-        # If the node has multiple users, duplicate the nodes
-        if len(node.users) != 1:
-            groups = split_nodes_on_path(graph, [node])
-            for g in groups:
-                partitions.insert(0, make_partition(g[0]))
-            continue
-        assert len(node.users) == 1, "dequantize_symmetric should only have one user"
         # Fuse the dequantize node with its user
-        user_node = next(iter(node.users))
-        group = search_group(user_node)
-        if group is None:
-            group = [node, user_node]
-            fused_partitions.append(group)
+        next_node = next(iter(node.users))
+        fused_nodes = [node]
+        fused = True
+        while not _is_gemm_op(next_node) and not _is_elementwise_op(next_node):
+            fused_nodes.append(next_node)
+            if len(next_node.users) != 1:
+                fused = False
+                groups = split_nodes_on_path(graph, fused_nodes)
+                for g in groups:
+                    partitions.insert(0, make_partition(g[0]))
+                break
+            next_node = next(iter(next_node.users))
+        if not fused:
+            continue
+        if (group := search_group(next_node)) is not None:
+            for n in reversed(fused_nodes):
+                if n not in group:
+                    user_node = next(iter(n.users))
+                    group.insert(group.index(user_node), n)
         else:
-            group.insert(group.index(user_node), node)
+            fused_nodes.append(next_node)
+            fused_partitions.append(fused_nodes)
 
     # Fuse nodes that appear earlier in the graph first
     node_order = {node: idx for idx, node in enumerate(graph.nodes)}
@@ -671,7 +680,7 @@ def _map_operation(node: Node, output_dir: str):
 def _compose_accelerator_param(node: Node, params: List, output_dir: str):
     accelerator_param = AcceleratorParam()
     accelerator_param.name = node.name
-    _set_tensor_field(accelerator_param.output, node, output_dir)
+    _set_tensor_field(accelerator_param.output, node, output_dir, True)
 
     if isinstance(params[0], MatrixParam):
         accelerator_param.matrix_param.CopyFrom(params[0])

src/quantized_training/codegen/mapping_utils.py
@@ -28,24 +28,25 @@ def _save_tensor(tensor, filename):
     print(f"Writing tensor to {filename}")
 
 
-def _set_tensor_field(field, node, output_dir=None):
+def _set_tensor_field(field, node, output_dir=None, is_output=False):
     assert isinstance(node, Node) and hasattr(node, 'value'), (
         f"Expected node {node} has value attribute. Make sure ShapeProp is called before mapping."
     )
 
     # If there is a reshape operation, we take the input from the reshape args
-    if (reshape := node.meta.get("reshape", None)) is not None:
-        field.permutation.node = reshape.name
-        field.permutation.opcode = reshape.target.__name__.split(".")[0]
-        field.permutation.dims.extend(
-            reshape.args[1]
-            if reshape.target == torch.ops.aten.permute.default
-            else reshape.args[1:]
-        )
-        field.permutation.output_shape.extend(node.shape)
-        if output_dir is not None:
-            _save_tensor(reshape.value, os.path.join(output_dir, f"{reshape.name}.bin"))
-        node = reshape.args[0]
+    if node.op != "call_module" or is_output:
+        if (reshape := node.meta.get("reshape", None)) is not None:
+            field.permutation.node = reshape.name
+            field.permutation.opcode = reshape.target.__name__.split(".")[0]
+            field.permutation.dims.extend(
+                reshape.args[1]
+                if reshape.target == torch.ops.aten.permute.default
+                else reshape.args[1:]
+            )
+            field.permutation.input_shape.extend(reshape.args[0].shape)
+            field.permutation.output_shape.extend(node.shape)
+            if not is_output:
+                node = reshape.args[0]
 
     # The reshape op can be further fused with a dequantize op.
     if (dq_scale := node.meta.get("dq_scale", None)) is not None:
@@ -72,8 +73,6 @@ def _set_tensor_field(field, node, output_dir=None):
     if (memory := node.meta.get("memory", None)) is not None:
         field.memory.partition = memory.partition_id
         field.memory.offset = memory.start
-    else:
-        print(f"Node {node.name} does not have memory attribute")
 
 
 def _set_repeated_field(field, value):
@@ -201,11 +200,12 @@ def _is_gemm_op(node: Node) -> bool:
 
 
 def _is_elementwise_op(node: Node) -> bool:
-    # Unary ops
+    # Unary ops: same input and output shape
     if (
         len(node.all_input_nodes) == 1
         and node.all_input_nodes[0].meta['val'].shape == node.meta['val'].shape
         and not _is_nop(node)
+        and node.target != torch.ops.aten.softmax.int
     ):
         return True
 
@@ -239,8 +239,7 @@ def _is_elementwise_op(node: Node) -> bool:
         # Maximum/Minimum ops
         torch.ops.aten.maximum.default,
         torch.ops.aten.minimum.default,
-        # Quantized ops
-        torch.ops.quantized_ops.calculate_mx_qparam,
+        # Quantization ops
         torch.ops.quantized_ops.dequantize_symmetric,
         torch.ops.quantized_ops.quantize_symmetric,
     ]
@@ -282,7 +281,7 @@ def map_elementwise(node, output_dir):
         else:
             param.other_scalar = node.args[1]
 
-    # TODO: Add a new field called dtype for quantize/dequantize operations
+    # TODO Add a new field called dtype for quantize/dequantize operations
     if len(node.args) > 2 and node.args[2] is not None:
         if node.target == torch.ops.quantized_ops.quantize_symmetric:
             param.opcode += "_to_" + node.args[2]
@@ -300,6 +299,7 @@ def map_reduce(node, output_dir):
         torch.ops.aten._softmax.default,
         torch.ops.aten.softmax.int,
         torch.ops.aten.sum.dim_IntList,
+        torch.ops.quantized_ops.calculate_mx_qparam,
     ]:
         return None
     param = ReduceParam()